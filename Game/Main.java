c4ab505dda12c39e086ddebbca246edfc4a0c3b4
Fareh Iqbal <fareh.legend@gmail.com>
2022-07-20 17:56:27 +0500

    Simple game, LMAO

---
+++ Game/Main.java
@@ -0,0 +1,21 @@
+package Game;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Player1 player = new Player1("Snake","Ariii" , 100);
+
+        // player.damagebyGun1();
+        player.damagebyGun1();
+        // player.damagebyGun2();
+        
+        Player2 player2 = new Player2("Noob", "Ak", 80, true);
+
+        player2.damagebyGun2();
+
+    }
+    
+
+    
+}
