61bfb96305c9c6a56d91ca25cfbe4acfa80e5a59
Fareh Iqbal <fareh.legend@gmail.com>
2022-08-03 01:15:25 +0500

    wroking

---
+++ Sol/contracts/Inbox.sol
@@ -0,0 +1,15 @@
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Inbox{
+    string public message;
+
+    constructor (string memory initialMessage) public {
+        message = initialMessage;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+
+    
+}
